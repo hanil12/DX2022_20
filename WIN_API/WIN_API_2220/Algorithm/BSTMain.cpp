572a889f7a0c29a600f019339999f70ad9f7a170
hanil12 <ekdrhaod@gmail.com>
2022-12-29 21:41:13 +0900

    DisjointSet

--- WIN_API/WIN_API_2220/Algorithm/BSTMain.cpp
+++ WIN_API/WIN_API_2220/Algorithm/BSTMain.cpp
@@ -17,18 +17,22 @@ int main()
 {
 	BinarySearchTree tree;
 
-	tree.Insert(50);
-	tree.Insert(30);
-	tree.Insert(70);
 	tree.Insert(20);
-	tree.Insert(54);
+	tree.Insert(30);
 	tree.Insert(40);
-	tree.Insert(85);
+	tree.Insert(50);
 	tree.Insert(52);
+	tree.Insert(54);
+	tree.Insert(70);
+	tree.Insert(85);
 
+	Node* min = tree.Min(tree._root);
+	Node* search = tree.Search(tree._root, 70);
+	Node* prev = tree.Previous(search);
+
+	tree.Delete(tree.Search(tree._root, 30));
 	tree.PrintTree(tree._root);
-
-	// Node* find = tree.Search(tree._root, 30);
+	
 
 	return 0;
 }
--- WIN_API/WIN_API_2220/Algorithm/BinarySearchTree.h
+++ WIN_API/WIN_API_2220/Algorithm/BinarySearchTree.h
@@ -29,6 +29,8 @@ public:
 	Node* Previous(Node* node);
 	Node* Next(Node* node);
 
+	void Delete(Node* node);
+	void Replace(Node* node1, Node* node2);
 
 	Node* _root = nullptr;
 };
